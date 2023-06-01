2fcac2e4cde8fc1a8118f97356b8ba8793905711
Sachith Dilhara <117737754+Sachithx@users.noreply.github.com>
2023-06-01 13:41:14 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-SHAREPOINT_USERNAME = "sachithxcviii@gmail.com"
+SHAREPOINT_USERNAME = "sachith.abeywickrama@rootcodelabs.com"
 SHAREPOINT_SITE = "https://kendrion.sharepoint.com/sites/TeamsProjectWise"
 SHAREPOINT_SITE_NAME = "TeamsProjectWise"
 SHAREPOINT_DOC = "Shared Documents"
