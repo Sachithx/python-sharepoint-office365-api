21ebe235c606229ae9fbde54f44b1bd1eb64a0f4
Sachithx <sachith.abeywickrama@rootcodelabs.com>
2023-06-01 15:56:39 +0530

    sharepy

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 SHAREPOINT_USERNAME = "sachith.abeywickrama@rootcodelabs.com"
-SHAREPOINT_SITE = "https://kendrion.sharepoint.com/sites/TeamsProjectWise"
+SHAREPOINT_URL = "https://kendrion.sharepoint.com/sites/TeamsProjectWise"
 SHAREPOINT_SITE_NAME = "TeamsProjectWise"
 SHAREPOINT_DOC = "Shared Documents"
